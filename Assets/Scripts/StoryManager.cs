c9c9d7300bb6ce9f4ccbaf632fcfbbd3b3ed61f7
Andykearlynode <services.nubianvr@gmail.com>
2021-10-04 15:20:49 +0000

    Added Page 1 and Page 2

---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryManager : MonoBehaviour
+{
+
+    public List<GameObject> allGameObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //allGameObjects = FindObjectOfType<GameObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
